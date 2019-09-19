359558b45011127720a43f8a38ef37ed3f67af9d
github username <github email address>
2019-09-19 14:08:23 -0400

    Done.

--- lib/project.rb
+++ lib/project.rb
@@ -1,5 +1,14 @@
 class Project
-  def initialize()
-    
+  attr_reader :title
+  attr_accessor :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
   end
 end
